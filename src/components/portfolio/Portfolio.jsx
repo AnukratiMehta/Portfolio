ad79346c845bc6d623d9d7d595c0c02843a66f7f
thephenomenonu <108215785+thephenomenonu@users.noreply.github.com>
2022-12-19 22:05:51 +0530

    added calculator

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/Calculator.png'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
@@ -11,9 +11,9 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: "X",
-    github: "https://github.com",
-    demo: "https://dribble.com/Alien_pixels"
+    title: "Calculator",
+    github: "https://github.com/AnukratiMehta/calculator.git",
+    demo: "https://anukratimehta.github.io/calculator/"
   },
   {
     id: 2,
@@ -60,16 +60,16 @@ const Portfolio = () => {
 
       <div className="container portfolio_container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({ id, image, title, github, demo }) => {
             return (
               <article key={id} className='portfolio_item'>
                 <div className="potfolio_image">
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
-                <div className="potfolio_cta">
-                  <a href={github}>Github</a>
-                  <a href={demo}>Live Demo</a>
+                <div className="portfolio_cta">
+                  <a href={github} target="_blank">Github</a>
+                  <a href={demo} target="_blank">Live Demo</a>
                 </div>
               </article>
             )
